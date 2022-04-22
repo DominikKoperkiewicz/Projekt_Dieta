6c7864f1721b395c9ef690c00e5d906c47bb610d
Dominik Koperkiewicz <254023@student.pwr.edu.pl>
2022-04-22 10:26:56 +0200

    Calendar entries added

--- Projekt_Dieta/Views/CalendarView.xaml.cs
+++ Projekt_Dieta/Views/CalendarView.xaml.cs
@@ -39,11 +39,57 @@ namespace Projekt_Dieta.Views
         {
             fromDate = fromDate.AddDays(7);
             LabelUpdate();
+
+            AddEntryBlock();
+        }
+
+        private void AddEntryBlock()
+        {
+            Button remove = new Button();
+            Button check = new Button();
+            TextBlock textblock = new TextBlock();
+            
+            textblock.Text = "Schabowy";
+
+            remove.Style = Resources["removeButton"] as Style;
+            check.Style = Resources["checkButton"] as Style;
+
+            remove.AddHandler(Button.ClickEvent, new RoutedEventHandler(Remove_Button_Click));
+
+            DockPanel dock = new DockPanel();
+            dock.Children.Add(textblock);
+            dock.Children.Add(remove);
+            dock.Children.Add(check);
+            mondayStack.Children.Add(dock);
+        }
+        
+        private void Remove_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button srcButton = e.Source as Button;
+            var parent = (UIElement)srcButton.Parent;
+            parent.Visibility = Visibility.Collapsed;
+        }
+
+
+        private void Check_Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Application.Current.Windows.OfType<MainWindow>().FirstOrDefault().Go_To_DishView();
         }
 
         private void LabelUpdate()
         {
             labelDate.Content = fromDate.ToString("dd.MM.yyyy") + " - " + fromDate.AddDays(6).ToString("dd.MM.yyyy");
+        }
+
+        private void RemoveAll()
+        {
+            mondayStack.Children.Clear();
+            tuesdayStack.Children.Clear();
+            wenesdayStack.Children.Clear();
+            thursdayStack .Children.Clear();
+            fridayStack.Children.Clear();
+            saturdayStack.Children.Clear();
+            sundayStack.Children.Clear();
         }
     }
 }
